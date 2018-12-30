c35964ea5331c4b5ece7ba96881ff9cf89bae4c4
dasitha@sp-solutions.biz <dasitha@sp-solutions.biz>
2018-12-30 22:32:55 +0530

    curd operation finished n tested

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DASITHA CURD';
+  description = 'Angular + SpringBoot';
+}
